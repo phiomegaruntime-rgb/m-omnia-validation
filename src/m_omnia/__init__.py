f178010dc88ea09fe56b080308c2c4b46c94e057
phiomegaruntime-rgb <phiomega.runtime@gmail.com>
2026-09-19 03:58:53 -0700

    Create __init__.py

---
+++ src/m_omnia/__init__.py
@@ -0,0 +1,8 @@
+"""M-OMNIA validation utilities.
+
+No physical system implementation is included until its original source and
+provenance are recovered.
+"""
+
+KAPPA_SECONDS_PER_ARC_UNIT = 3.19275
+
